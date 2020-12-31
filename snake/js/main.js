bc99452039b8cbc50f561293368475267e8c5c32
Wallace H Wu <wallace_h_wu@wistron.com>
2020-12-31 10:43:34 +0800

    update

--- snake/js/main.js
+++ snake/js/main.js
@@ -1,49 +1,20 @@
-// import {getDirection} from './operation.js';
-// import {snakeSpeed, updateSnake, renderSnake, getSnakeHead, snakeBodyIntersection} from './snake.js';
-// import {checkOnSnakeBody} from './common/util.js';
-import {snakeA, snakeB} from './role/snake.js';
 import {initAllFood, updateAllFood, renderAllFood} from './role/food.js';
-// import {foodA, foodB} from './role/food.js';
+import {initAllSnake, checkAllSnakeDead, updateAllSnakePosition, renderAllSnake} from './role/snake.js';
 import {map} from './role/map.js';
-import {gameJudge} from './judge/judge.js';
 
-const snakeList = Array.prototype.concat(...Object.values(gameJudge.getJudgeData('getAllSnake')));
 const snakeSpeed = 1;
 let lastRenderTime = 2;
 
-// 取當前的蛇頭座標是否超出邊界 & 蛇頭是否撞到自己的身體
-// const checkGameOver = () => {
-//
-//     // gameOver = snakeA.checkSnakeGameOver(snakeJudgeHandler, snakeA.getSnakeHead())
-//     //     || snakeB.checkSnakeGameOver(snakeJudgeHandler, snakeB.getSnakeHead())
-//
-//     // gameOver = snakeA.snakeBodyIntersection(checkOnSnakeBody,{ ignoreHead: false });
-// }
-
 const update = () => {
-    // gameJudge.updateGameRenderData();
-    // foodA.updateFood(snakeList);
-    // foodB.updateFood(snakeList);
-    updateAllFood(snakeList);
-    snakeA.updateSnakePosition();
-    snakeB.updateSnakePosition();
-    snakeA.checkSnakeDead();
-    snakeB.checkSnakeDead();
-    // snakeA.checkSnakeGameOver();
-    // snakeB.checkSnakeGameOver();
-    // console.log(snakeA.checkSelf(gameJudge));
-    // snakeB.checkSelf(gameJudge);
-    // checkGameOver();
+    updateAllFood();
+    updateAllSnakePosition();
+    checkAllSnakeDead();
 }
 
 const render = () => {
     map.gameMap.innerHTML = '';
-    // gameJudge.doGameRender();
-    // foodA.renderFood(map.gameMap);
-    // foodB.renderFood(map.gameMap);
     renderAllFood();
-    snakeA.renderSnake(map.gameMap);
-    snakeB.renderSnake(map.gameMap);
+    renderAllSnake();
 }
 
 const main = (currentTime) => {
@@ -65,8 +36,6 @@ const main = (currentTime) => {
     update();
 }
 
-// gameJudge.initGameRender();
-snakeA.initListenerOperation();
-snakeB.initListenerOperation();
 initAllFood();
+initAllSnake();
 window.requestAnimationFrame(main);
