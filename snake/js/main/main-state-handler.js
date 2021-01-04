2e85d1ea24dc61a98b8ed33933557c31718fe5d8
Wallace H Wu <wallace_h_wu@wistron.com>
2021-01-04 22:51:43 +0800

    update

---
+++ snake/js/main/main-state-handler.js
@@ -0,0 +1,19 @@
+const mainStatusHandler = (function () {
+    const handler = {};
+
+    handler.gameStart = function () {
+
+    };
+
+    handler.gamePause = function () {
+
+    };
+
+    handler.gameFinish = function () {
+
+    };
+})();
+
+export {
+    mainStatusHandler
+}
